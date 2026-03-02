df4099bb2d7bb9fc7c56747497adb635da350a03
tenkus47 <tenkus47@gmail.com>
2026-03-02 17:36:04 +0530

    Redirect root route to /chat in app.js

--- src/app.js
+++ src/app.js
@@ -10,7 +10,7 @@ app.use(express.json())
 app.use(express.static(path.join(__dirname, '..', 'public')))
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.redirect('/chat')
 })
 
 app.get('/chat', (req, res) => {
